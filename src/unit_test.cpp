ea587ea23df442d7fe528afac01eaf27af9faed9
James Wu <jameswu62@yahoo.co.nz>
2021-06-10 09:30:16 +1200

    first init

---
+++ src/unit_test.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "wearleveling.h"
+
+namespace wearlevelingLibraryTest
+{
+    class wearlevelingLibraryTest:public::testing::Test
+    {
+        public:
+        private:
+        protected:
+
+        wearlevelingLibraryTest(){}
+        virtual ~wearlevelingLibraryTest(){}
+
+        virtual void SetUp(){}
+        virtual void TearDown(){}
+    };
+
+    TEST_F(wearlevelingLibraryTest, dummyTest)
+    {
+        ASSERT_EQ(0,wearleveling_returnOne());
+    }
+}
---
+++ src/wearleveling.c
@@ -0,0 +1,6 @@
+#include "wearleveling.h"
+
+uint8_t wearleveling_returnOne(void)
+{
+    return 1;
+}
