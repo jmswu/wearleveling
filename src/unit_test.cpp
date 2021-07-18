e7410439b7b4fd6e243b952d38dab19455b6e88e
James Wu <jameswu62@yahoo.co.nz>
2021-07-18 13:36:17 +1200

    added v2 save_1 test

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -509,6 +509,7 @@ namespace wearlevelingLibraryTest
 
     TEST_F(wearlevelingLibraryTest, save_1)
     {
+        /* common data */
         wearleveling_params_typeDef params = 
         {
             .pageCapacityInByte = 1024,
@@ -520,9 +521,22 @@ namespace wearlevelingLibraryTest
 
         uint8_t dummy_data [] = {0x11};
 
+        /* v1 test */
         mock_pageErase();
         wearleveling.init(&params);
         wearleveling.save(dummy_data);
+
+        ASSERT_EQ(0x34, page[0]);
+        ASSERT_EQ(0x12, page[1]); 
+
+        ASSERT_EQ(0x11, page[2]);
+        ASSERT_EQ(0x55, page[3]); 
+
+        /* v2 test */
+        mock_pageErase();
+        wearleveling_state_typeDef wearlevelingState;
+        const wearleveling_handle_typeDef handle = wearleveling_v2_construct(&wearlevelingState, &params);
+        wearleveling_v2_save(handle, dummy_data);
 
         ASSERT_EQ(0x34, page[0]);
         ASSERT_EQ(0x12, page[1]); 
